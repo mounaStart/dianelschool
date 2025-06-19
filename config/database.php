d5b1c3fa5a6a53be1b904f198fe9c1854d9c22ab
mounaStart <83177253+mounaStart@users.noreply.github.com>
2025-06-19 13:18:29 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -63,20 +63,23 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
-        ],
+       'pgsql' => [
+    'driver' => 'pgsql',
+    'url' => env('DATABASE_URL'),
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'charset' => 'utf8',
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'search_path' => 'public',
+    'sslmode' => 'require', // Changé de 'prefer' à 'require'
+    'options' => [
+        PDO::PGSQL_ATTR_SSL_CERT_MODE => PDO::SSL_CERT_REQUIRE, // Ajouté
+    ],
+    ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
