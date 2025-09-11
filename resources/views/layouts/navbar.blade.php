0304b2f5c7021cd33c70aebd312c04e0e4103ec4
mounaStart <83177253+mounaStart@users.noreply.github.com>
2025-09-11 19:59:11 +0200

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
   <!-- Barre de navigation -->
     <nav class="navbar navbar-expand-md navbar-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#">RimSchool</a>
+            <a class="navbar-brand" href="#">DianelSchool</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
